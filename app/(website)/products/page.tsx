8df2f978e465af30058882493f0ea98b63b017f7
Atif Hameed <atifhameed11312@gmail.com>
2025-11-28 00:59:16 +0500

    new udpates and fix bugs

--- app/(website)/products/page.tsx
+++ app/(website)/products/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic"
+export const revalidate = 0
+
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import ProductsClientPage from "@/components/products/products-client-page"
